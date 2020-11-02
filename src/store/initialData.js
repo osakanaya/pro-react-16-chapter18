b8fa3f92e044ea089c697395b75020e597a77389
sashida <sashida@primagest.co.jp>
2020-11-03 03:12:06 +0900

    p.532

---
+++ src/store/initialData.js
@@ -0,0 +1,13 @@
+import { PRODUCTS, SUPPLIERS } from "./dataTypes";
+
+export const initialData = {
+    [PRODUCTS]: [
+        { id: 1, name: "Kayak", category: "Watersports", price: 275 },
+        { id: 2, name: "Lifejacket", category: "Watersports", price: 48.95 },
+        { id: 3, name: "Soccer Ball", category: "Soccer", price: 19.50 }
+    ],
+    [SUPPLIERS]: [
+        { id: 1, name: "Surf Dudes", city: "San Jose", products: [1, 2] },
+        { id: 2, name: "Field Supplies", city: "New York", products: [3] },
+    ]
+};
