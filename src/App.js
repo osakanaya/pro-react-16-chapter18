a37c62f03b3875abda9b5f124e221de16019de5a
sashida <sashida@primagest.co.jp>
2020-11-05 14:25:02 +0900

    p.573

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import dataStore from "./store";
+import dataStore, { deleteProduct } from "./store";
 import { Selector } from "./Selector";
 import { ProductDisplay } from "./ProductDisplay";
 import { SupplierDisplay } from "./SupplierDisplay";
 import { StoreAccess } from "./store/StoreAccess";
+import { CustomConnector, CustomConnectorProvider } from "./store/CustomConnector";
+import { startEditingProduct } from "./store/stateActions";
+import { ProductTable } from "./ProductTable";
+
+const selectors = {
+  products: (store) => store.modelData.products
+};
+
+const dispatchers = {
+  editCallback: startEditingProduct,
+  deleteCallback: deleteProduct
+};
+
 
 export default class App extends Component {
   render() {
@@ -20,6 +33,15 @@ export default class App extends Component {
               <SupplierDisplay name="Suppliers" />
             </Selector>
           </Provider>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <CustomConnectorProvider dataStore={ dataStore }>
+            <CustomConnector selectors={ selectors } dispatchers={ dispatchers }>
+              <ProductTable />
+            </CustomConnector>
+          </CustomConnectorProvider>
         </div>
       </div>
     </div>
