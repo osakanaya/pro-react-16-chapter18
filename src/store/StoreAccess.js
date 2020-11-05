a7fec847fab55298e0a62a42d26b62d49a664c63
sashida <sashida@primagest.co.jp>
2020-11-05 13:53:43 +0900

    p.566

--- src/store/StoreAccess.js
+++ src/store/StoreAccess.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
+import store from ".";
 
 export class StoreAccess extends Component {
+    constructor(props) {
+        super(props);
+        this.selectors = {
+            product: (storeState) => storeState.modelData.products[0],
+            state: (storeState) => storeState.stateData
+        };
+    }
+
     render() {
         return <div className="bg-info">
             <pre className="text-white">
-                { JSON.stringify(this.props.store.getState(), null, 2) }
+                { JSON.stringify(this.selectData(), null, 2) }
             </pre>
         </div>
+    }
+
+    selectData() {
+        let storeState = this.props.store.getState();
+
+        return Object.entries(this.selectors).map(([k, v]) => [k, v(storeState)])
+            .reduce((result, [k, v]) => ({ ...result, [k]: v}), {});
     }
 }
