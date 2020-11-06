ad54dfc3fd05c2a0098be08ad86a9399b1f77a28
sashida <sashida@primagest.co.jp>
2020-11-06 11:33:33 +0900

    p.583-2

--- src/store/asyncEnhancer.js
+++ src/store/asyncEnhancer.js
@@ -1,17 +1,13 @@
-export function asyncEnhancer(delay) {
-    return function(createStoreFunction) {
-        return function(...args) {
-            const store = createStoreFunction(...args);
+export const asyncEnhancer = delay => createStoreFunction => (...args) => {
+    const store = createStoreFunction(...args);
 
-            return {
-                ...store,
-                dispatchAsync: (action) => new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                        store.dispatch(action);
-                        resolve();
-                    }, delay);
-                })
-            };
-        }
-    }
-}
+    return {
+        ...store,
+        dispatchAsync: (action) => new Promise((resolve, reject) => {
+            setTimeout(() => {
+                store.dispatch(action);
+                resolve();
+            }, delay);
+        })
+    };
+};
