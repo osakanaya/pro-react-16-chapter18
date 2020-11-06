bf9841ee486e8f623722e2e486cff86300ad9861
sashida <sashida@primagest.co.jp>
2020-11-06 23:29:23 +0900

    p.586

--- src/store/TableConnector.js
+++ src/store/TableConnector.js
@@ -4,14 +4,19 @@ import { deleteProduct, deleteSupplier } from "./modelActionCreators";
 import { PRODUCTS, SUPPLIERS } from "./dataTypes";
 
 export const TableConnector = (dataType, presentationComponent) => {
-    const mapStateToProps = (storeData) => ({
-        products: storeData.modelData[PRODUCTS],
-        suppliers: storeData.modelData[SUPPLIERS].map(supp => ({
-            ...supp,
-            products: supp.products.map(id => 
-                storeData.modelData[PRODUCTS].find(p => p.id === Number(id)) || id).map(val => val.name || val)
-        }))
-    });
+    const mapStateToProps = (storeData, ownProps) => {
+        if (!ownProps.needSuppliers) {
+            return { products: storeData.modelData[PRODUCTS] };
+        } else {
+            return {
+                suppliers: storeData.modelData[SUPPLIERS].map(supp => ({
+                    ...supp,
+                    products: supp.products.map(id => 
+                        storeData.modelData[PRODUCTS].find(p => p.id === Number(id)) || id).map(val => val.name || val)
+                }))
+            }
+        }
+    };
 
     const mapDispatchToProps = {
         editCallback: dataType === PRODUCTS ? startEditingProduct : startEditingSupplier,
