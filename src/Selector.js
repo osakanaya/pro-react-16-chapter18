69ae12831693990e7aba875f1d38d1ef69991eeb
sashida <sashida@primagest.co.jp>
2020-11-10 01:34:05 +0900

    p.601

--- src/Selector.js
+++ src/Selector.js
@@ -7,18 +7,21 @@ export class Selector extends Component {
 
     renderMessage = (msg) => <h5 className="bg-info text-white m-2 p-2">{ msg }</h5>
 
-   render() {
+    render() {
         return <Router>
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-2">
+                        <div><Link to="/data">Data</Link></div>
+                        <div><Link to="/data/">Data - Slash</Link></div>
                         <div><Link to="/data/one">Link #1</Link></div>
                         <div><Link to="/data/two">Link #2</Link></div>
                         <div><Link to="/people/bob">Bob</Link></div>
                     </div>
                     <div className="col">
-                        <Route path="/data" render={ () => this.renderMessage("Route #1" ) } />
-                        <Route path="/data/two" render={ () => this.renderMessage("Route #2" ) } />
+                        <Route path="/data" exact={ true } render={ () => this.renderMessage("Route #1" ) } />
+                        <Route path="/data/" strict={ true } render={ () => this.renderMessage("Route #2" ) } />
+                        <Route path="/data/two" render={ () => this.renderMessage("Route #3" ) } />
                     </div>
                 </div>            
             </div>
