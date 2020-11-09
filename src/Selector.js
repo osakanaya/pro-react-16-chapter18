9307908f66e5f5d3047129ee1b204342fad3991c
sashida <sashida@primagest.co.jp>
2020-11-10 01:52:11 +0900

    p.617

--- src/Selector.js
+++ src/Selector.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter as Router, NavLink, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, NavLink, Route, Switch, Redirect } from "react-router-dom";
 import { ProductDisplay } from "./ProductDisplay";
 import { SupplierDisplay } from "./SupplierDisplay";
 
@@ -12,16 +12,15 @@ export class Selector extends Component {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-2">
-                        <NavLink className="m-2 btn btn-block btn-primary" activeClassName="active" exact={ true } to="/">Default URL</NavLink>
-                        <NavLink className="m-2 btn btn-block btn-primary" activeClassName="active" to="/products">Products</NavLink>
-                        <NavLink className="m-2 btn btn-block btn-primary" activeClassName="active" to="/suppliers">Suppliers</NavLink>
-                        <NavLink className="m-2 btn btn-block btn-primary" activeClassName="active" to="/old/data">Old Link</NavLink>
+                        <NavLink className="m-2 btn btn-block btn-primary" 
+                            activeClassName="active" to="/products">Products</NavLink>
+                        <NavLink className="m-2 btn btn-block btn-primary" 
+                            activeClassName="active" to="/suppliers">Suppliers</NavLink>
                     </div>
                     <div className="col">
                         <Switch>
                             <Route path="/products" component={ ProductDisplay } />
                             <Route path="/suppliers" component={ SupplierDisplay } />
-                            <Redirect from="/old/data" to="/suppliers" />
                             <Redirect to="/products" />
                         </Switch>
                     </div>
