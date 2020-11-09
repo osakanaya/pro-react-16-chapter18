a7700100a9317c8b110bcf31cbd10afb14c527ff
sashida <sashida@primagest.co.jp>
2020-11-10 01:41:51 +0900

    p.606

--- src/Selector.js
+++ src/Selector.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch, Redirect } from "react-router-dom";
 import { ProductDisplay } from "./ProductDisplay";
 import { SupplierDisplay } from "./SupplierDisplay";
 
 export class Selector extends Component {
 
     renderMessage= (msg) => <h5 className="bg-info text-white m-2 p-2">{ msg }</h5>
+
     render() {
         return <Router>
             <div className="container-fluid">
@@ -19,7 +20,7 @@ export class Selector extends Component {
                         <Switch>
                             <Route path="/products" component={ ProductDisplay } />
                             <Route path="/suppliers" component={ SupplierDisplay } />
-                            <Route render={ () => this.renderMessage("Fallback Route") } />
+                            <Redirect to="/products" />
                         </Switch>
                     </div>
                 </div>            
