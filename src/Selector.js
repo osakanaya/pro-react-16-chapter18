1589ebaa58f4ff4a69b187f0e8e201052d1ccf78
sashida <sashida@primagest.co.jp>
2020-11-16 07:57:30 +0900

    p.620

--- src/Selector.js
+++ src/Selector.js
@@ -2,10 +2,9 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, NavLink, Route, Switch, Redirect } from "react-router-dom";
 import { ProductDisplay } from "./ProductDisplay";
 import { SupplierDisplay } from "./SupplierDisplay";
+import { RouteInfo } from "./routing/RouteInfo";
 
 export class Selector extends Component {
-
-    renderMessage= (msg) => <h5 className="bg-info text-white m-2 p-2">{ msg }</h5>
 
     render() {
         return <Router>
@@ -16,11 +15,14 @@ export class Selector extends Component {
                             activeClassName="active" to="/products">Products</NavLink>
                         <NavLink className="m-2 btn btn-block btn-primary" 
                             activeClassName="active" to="/suppliers">Suppliers</NavLink>
+                        <NavLink className="m-2 btn btn-block btn-primary"
+                            activeClassName="active" to="/info">Route Info</NavLink>
                     </div>
                     <div className="col">
                         <Switch>
                             <Route path="/products" component={ ProductDisplay } />
                             <Route path="/suppliers" component={ SupplierDisplay } />
+                            <Route path="/info" component={ RouteInfo } />
                             <Redirect to="/products" />
                         </Switch>
                     </div>
