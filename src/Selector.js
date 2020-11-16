fab298eda3a7625511cd5909dc8960c7e4a78984
sashida <sashida@primagest.co.jp>
2020-11-17 00:38:24 +0900

    p.641

--- src/Selector.js
+++ src/Selector.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect, withRouter, Prompt } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect, Prompt } from "react-router-dom";
 import { ProductDisplay } from "./ProductDisplay";
 import { SupplierDisplay } from "./SupplierDisplay";
-import { RouteInfo } from "./routing/RouteInfo";
+//import { RouteInfo } from "./routing/RouteInfo";
 import { ToggleLink } from "./routing/ToggleLink";
 import { CustomPrompt } from "./routing/CustomPrompt";
 
-const RouterInfoHOC = withRouter(RouteInfo);
+//const RouterInfoHOC = withRouter(RouteInfo);
 
 export class Selector extends Component {
 
@@ -31,26 +31,30 @@ export class Selector extends Component {
     }
 
     render() {
+
+        const routes = React.Children.map(this.props.children, child => ({
+            component: child,
+            name: child.props.name,
+            url: `/${child.props.name.toLowerCase()}`
+        }));
+
         return <Router getUserConfirmation= { this.customGetUserConfirmation }>
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-2">
-                        <ToggleLink to="/products">Products</ToggleLink>
-                        <ToggleLink to="/suppliers">Suppliers</ToggleLink>
-                        <ToggleLink to="/info/match">Match</ToggleLink>
-                        <ToggleLink to="/info/location">Location</ToggleLink>
-                        <ToggleLink to="/info" exact={ true }>All Info</ToggleLink>
+                        { routes.map(r => <ToggleLink key={ r.url } to={ r.url }>
+                            { r.name }
+                        </ToggleLink>)}
                     </div>
                     <div className="col">
                         <CustomPrompt show={ this.state.showPrompt } message={ this.state.message } 
                             callback={ this.state.callback } />
                         <Prompt message={ loc => `Do you want to navigate to ${loc.pathname}` } />
-                        <RouterInfoHOC />
                         <Switch>
-                            <Route path="/products" component={ ProductDisplay } />
-                            <Route path="/suppliers" component={ SupplierDisplay } />
-                            <Route path="/info/:dataType?" component={ RouteInfo } />
-                            <Redirect to="/products" />
+                            { routes.map( r => 
+                                <Route key={ r.url } path={ r.url } render={ () => r.component } />
+                            )}
+                            <Redirect to={ routes[0].url } />
                         </Switch>
                     </div>
                 </div>            
