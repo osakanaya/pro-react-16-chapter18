a20b61928f8c64a55ff2bdf979f424fb485b33ca
sashida <sashida@primagest.co.jp>
2020-11-24 08:01:49 +0900

    p.691

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -2,5 +2,9 @@ var data = require("../../restData")();
 
 module.exports = {
     products: () => data.products,
-    suppliers: () => data.suppliers
+    suppliers: () => data.suppliers.map(s => ({
+        ...s, products: () => s.products.map(id =>
+            data.products.find(p => p.id === Number(id))
+        )
+    }))
 };
